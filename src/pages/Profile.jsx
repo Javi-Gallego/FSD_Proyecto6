f78a7c88ffd084695c220a276b5402eed90a47cc
Javi-Gallego <galgar@gmail.com>
2024-03-22 01:01:13 +0100

    feat: create profile page but not finished

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+import "./Profile.css"
+
+export const Profile = () => {
+    return (
+        <div className="profileDesign">
+        <h1>Profile</h1>
+        <p>My name is John Doe</p>
+        </div>
+    )
+}
